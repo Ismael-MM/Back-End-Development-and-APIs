09d09073aabaafbc70c8eac627471ad1687b9f67
ismael <isMontelongo@outlook.com>
2024-01-22 21:56:41 +0000

    7.1

--- myApp.js
+++ myApp.js
@@ -67,7 +67,7 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  Person.findById()({_id: personId}, function (err, data) {
+  Person.findById()(personId, function (err, data) {
     if (err) return console.log(err);
     done(null, data);
   })
